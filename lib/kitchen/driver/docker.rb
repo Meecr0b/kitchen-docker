ee04c85fd193f523f4d672fbaa07cc6998b5320a
Meecr0b <Meecr0b@users.noreply.github.com>
2018-04-17 10:00:57 +0200

    removed package 'which' from sles

--- lib/kitchen/driver/docker.rb
+++ lib/kitchen/driver/docker.rb
@@ -223,7 +223,7 @@ module Kitchen
         when 'opensuse', 'sles'
           <<-eos
             ENV container docker
-            RUN zypper install -y sudo openssh which curl
+            RUN zypper install -y sudo openssh curl
             RUN [ -f "/etc/ssh/ssh_host_rsa_key" ] || ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -N ''
             RUN [ -f "/etc/ssh/ssh_host_dsa_key" ] || ssh-keygen -t dsa -f /etc/ssh/ssh_host_dsa_key -N ''
           eos
